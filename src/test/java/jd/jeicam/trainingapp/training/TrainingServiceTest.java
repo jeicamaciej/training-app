1cfc792bd3790c60a41521c5cc543ee4406ffef1
jeicamaciej <mkalafar1@gmail.com>
2020-02-28 19:24:57 +0100

    added more tests for training class

--- src/test/java/jd/jeicam/trainingapp/training/TrainingServiceTest.java
+++ src/test/java/jd/jeicam/trainingapp/training/TrainingServiceTest.java
@@ -2,16 +2,12 @@ package jd.jeicam.trainingapp.training;
 
 import jd.jeicam.trainingapp.exercise.Exercise;
 import jd.jeicam.trainingapp.exercise.ExerciseRepository;
-import org.junit.After;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import javax.validation.constraints.AssertTrue;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -55,6 +51,8 @@ public class TrainingServiceTest {
 
         //when
         trainingService.addTraining(null);
+
+        //then
         verify(trainingRepository).save(null);
         verifyNoMoreInteractions(trainingRepository);
     }
@@ -74,6 +72,22 @@ public class TrainingServiceTest {
         verify(trainingRepository).findById(id);
         assertThat(optionalTraining).contains(training);
         verifyNoMoreInteractions(trainingRepository);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getNullTrainingShouldThrowException() {
+        //given
+        Training training = null;
+        Long trainingId = 88L;
+        when(trainingRepository.findById(trainingId)).thenThrow(IllegalArgumentException.class);
+
+        //when
+        trainingService.getTraining(trainingId);
+
+        //then
+        verify(trainingRepository).findById(trainingId);
+        verifyNoMoreInteractions(trainingRepository);
+
     }
 
     @Test
@@ -134,9 +148,10 @@ public class TrainingServiceTest {
 
         //then
         assertTrue(isExerciseAdded);
-        assertTrue(training.getExercises().contains(exercise));
-        assertEquals(1, training.getExercises().size());
+        //assertTrue(training.getExercises().contains(exercise));
+        //assertEquals(1, training.getExercises().size());
         assertTrue(isTrainingPresent && isExercisePresent);
+        assertThat(training.getExercises()).containsExactly(exercise);
 
         verify(trainingRepository).save(training);
         verify(trainingRepository, times(2)).findById(trainingId);
@@ -145,4 +160,116 @@ public class TrainingServiceTest {
         verifyNoMoreInteractions(exerciseRepository);
     }
 
+    @Test
+    public void shouldReturnFalseIfBothOptionalsEmpty() {
+        //given
+        Long trainingId = 88L;
+        Long exerciseId = 44L;
+        Optional<Training> optionalTraining = Optional.empty();
+        Optional<Exercise> optionalExercise = Optional.empty();
+
+        when(trainingRepository.findById(trainingId)).thenReturn(optionalTraining);
+        when(exerciseRepository.findById(exerciseId)).thenReturn(optionalExercise);
+
+        //when
+        boolean isTrainingPresent = trainingRepository.findById(trainingId).isPresent();
+        boolean isExercisePresent = exerciseRepository.findById(exerciseId).isPresent();
+        boolean isExerciseAddedToTraining = trainingService.addExerciseToTraining(trainingId, exerciseId);
+
+        //then
+        assertFalse(isExerciseAddedToTraining);
+        assertFalse(isTrainingPresent && isExercisePresent);
+        //verify(trainingRepository, never()).save(optionalTraining.get());
+        verify(trainingRepository, times(2)).findById(trainingId);
+        verify(exerciseRepository, times(2)).findById(exerciseId);
+        verifyNoMoreInteractions(trainingRepository);
+        verifyNoMoreInteractions(exerciseRepository);
+    }
+
+    @Test
+    public void shouldReturnFalseIfTrainingIsEmpty() {
+        //given
+        Long trainingId = 88L;
+        Long exerciseId = 44L;
+        Optional<Training> optionalTraining = Optional.empty();
+        Exercise exercise = new Exercise();
+        exercise.setId(exerciseId);
+
+        when(trainingRepository.findById(trainingId)).thenReturn(optionalTraining);
+        when(exerciseRepository.findById(exerciseId)).thenReturn(Optional.of(exercise));
+
+        //when
+        boolean isTrainingPresent = trainingRepository.findById(trainingId).isPresent();
+        boolean isExercisePresent = exerciseRepository.findById(exerciseId).isPresent();
+        boolean isExerciseAddedToTraining = trainingService.addExerciseToTraining(trainingId, exerciseId);
+
+        //then
+        assertFalse(isExerciseAddedToTraining);
+        assertFalse(isTrainingPresent && isExercisePresent);
+        //verify(trainingRepository, never()).save(optionalTraining.get());
+        verify(trainingRepository, times(2)).findById(trainingId);
+        verify(exerciseRepository, times(2)).findById(exerciseId);
+        verifyNoMoreInteractions(trainingRepository);
+        verifyNoMoreInteractions(exerciseRepository);
+    }
+
+    @Test
+    public void shouldReturnFalseIfExerciseEmpty() {
+        //given
+        Long trainingId = 88L;
+        Long exerciseId = 44L;
+        Training training = new Training();
+        training.setId(trainingId);
+
+        Optional<Exercise> optionalExercise = Optional.empty();
+
+        when(trainingRepository.findById(trainingId)).thenReturn(Optional.of(training));
+        when(exerciseRepository.findById(exerciseId)).thenReturn(optionalExercise);
+
+        //when
+        boolean isTrainingPresent = trainingRepository.findById(trainingId).isPresent();
+        boolean isExercisePresent = exerciseRepository.findById(exerciseId).isPresent();
+        boolean isExerciseAddedToTraining = trainingService.addExerciseToTraining(trainingId, exerciseId);
+
+        //then
+        assertFalse(isExerciseAddedToTraining);
+        assertFalse(isTrainingPresent && isExercisePresent);
+        //verify(trainingRepository, never()).save(optionalTraining.get());
+        verify(trainingRepository, times(2)).findById(trainingId);
+        verify(exerciseRepository, times(2)).findById(exerciseId);
+        verifyNoMoreInteractions(trainingRepository);
+        verifyNoMoreInteractions(exerciseRepository);
+    }
+
+    @Test
+    public void shouldReturnExerciseList() {
+        //given
+        Training training = new Training();
+        Training training1 = new Training();
+        List<Training> trainings = new ArrayList<>();
+        when(trainingRepository.findAll()).thenReturn(trainings);
+
+        //when
+        List<Training> newTrainings = new ArrayList<>(trainingService.getTrainings());
+
+        //then
+        assertEquals(trainings, newTrainings);
+        verify(trainingRepository).findAll();
+        verifyNoMoreInteractions(trainingRepository);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIfNullTraining() {
+        //given
+        List<Training> trainings = new ArrayList<>();
+        trainings.add(null);
+        when(trainingRepository.findAll()).thenThrow(IllegalArgumentException.class);
+
+        //when
+        trainingService.getTrainings();
+
+        //then
+        verify(trainingRepository).findAll();
+        verifyNoMoreInteractions(trainingRepository);
+    }
 }
